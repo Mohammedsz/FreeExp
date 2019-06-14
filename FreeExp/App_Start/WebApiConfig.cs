707c4aebb6f7bb68619f81d5389b8238d5853d05
Mohammedsz <51034844+Mohammedsz@users.noreply.github.com>
2019-06-14 22:45:51 +0200

    web api return

--- FreeExp/App_Start/WebApiConfig.cs
+++ FreeExp/App_Start/WebApiConfig.cs
@@ -1,7 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
 
 namespace FreeExp
 {
@@ -10,7 +16,12 @@ namespace FreeExp
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.Formatters.JsonFormatter.SerializerSettings.Converters.Add(new StringEnumConverter());
+            config.Formatters.JsonFormatter.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+            config.Formatters.JsonFormatter.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Local;
 
+            config.Formatters.Insert(0,
+                new JsonPropertyCaseFormatter(config.Formatters.JsonFormatter.SerializerSettings));
             // Web API routes
             config.MapHttpAttributeRoutes();
 
@@ -19,6 +30,42 @@ namespace FreeExp
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+        }
+    }
+
+    public class JsonPropertyCaseFormatter : JsonMediaTypeFormatter
+    {
+        private readonly JsonSerializerSettings globalSerializerSettings;
+
+        public JsonPropertyCaseFormatter(JsonSerializerSettings globalSerializerSettings)
+        {
+            this.globalSerializerSettings = globalSerializerSettings;
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/json"));
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/javascript"));
+        }
+
+        public override MediaTypeFormatter GetPerRequestFormatterInstance(
+            Type type,
+            HttpRequestMessage request,
+            MediaTypeHeaderValue mediaType)
+        {
+            var formatter = new JsonMediaTypeFormatter
+            {
+                SerializerSettings = globalSerializerSettings
+            };
+
+            IEnumerable<string> values;
+
+            var result = request.Headers.TryGetValues("X-JsonResponseCase", out values)
+                ? values.First()
+                : "Pascal";
+
+            formatter.SerializerSettings.ContractResolver =
+                result.Equals("Camel", StringComparison.InvariantCultureIgnoreCase)
+                    ? new CamelCasePropertyNamesContractResolver()
+                    : new DefaultContractResolver();
+
+            return formatter;
         }
     }
 }
